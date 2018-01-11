a8aaf95149d226e2b0716183bc66ec49ea7b4ebe
Maximilian Khan <maxjkh@gmail.com>
2018-01-11 00:24:26 -0800

    getting there

--- data-analysis.py
+++ data-analysis.py
@@ -57,33 +57,48 @@ def get_peaks_and_times(data, time):
     # Compute gradients to find where derivitives are zero.
     gradient_of_data = np.gradient(data)
 
-    count = 0
+    counter = 0
     values_for_peaks = []
     times_of_peaks = []
+    peak_indexes = []
     for x in gradient_of_data:
         # Computing the gradients gives values of 0.0 inaccurately. But, within a range of values close to zero, we get our desired points. 
         # Experiments reveal that finding a signal from its troph is more accurate than its peaks.
         # So, we will look below our threshhold of 50 mV for points that are close to a gradient of zero.
-        if (np.abs(x) < 0.0045) and (data[count] < -0.05):
-            times_of_peaks.append(time[count])
-            values_for_peaks.append(data[count])
-        count += 1
+        if (np.abs(x) < 0.0045) and (data[counter] < -0.05):
+            times_of_peaks.append(time[counter])
+            values_for_peaks.append(data[counter])
+            peak_indexes.append(counter)
+        counter += 1
 
-    return values_for_peaks, times_of_peaks
+    return values_for_peaks, times_of_peaks, peak_indexes
 
 def get_non_null_lengths_and_times(data, time):
     count = 0 
     non_null_length_values = []
     times_of_lengths = []
+    length_indexes = []
     for x in length_data:
         # Since the length channel doesn't actually hold values constant, I tested that this was a fair 
         # baseline value to judge off-of. 
         if x > 0.0000025:
             non_null_length_values.append(x)
             times_of_lengths.append(time[count])
+            length_indexes.append(count)
         count += 1
 
-    return non_null_length_values, times_of_lengths
+    return non_null_length_values, times_of_lengths, length_indexes
+
+def get_peaks_during_ramps(peak_indexes, length_indexes, amp_data, time):
+    peaks_during_ramp = []
+    good_peak_times = []
+    for x in peak_indexes:
+        for y in length_indexes:
+            if x == y:
+                peaks_during_ramp.append(amp_data[x])
+                good_peak_times.append(time[x])
+
+    return peaks_during_ramp, good_peak_times
 
 #====================================================================================
 # Main
@@ -100,15 +115,20 @@ amp_data = amp_data[:UPPER_BOUND]
 length_data = length_data[:UPPER_BOUND]
 
 # Get the instantaneous frequency of our signal, and an array containing a series of our time for which the data occurs over. 
+# I will most likely have to get the indexes for the inst_freq peaks as well...
 inst_freq, time = get_instantaneous_frequency(amp_data)
+
 # Get the peaks and the times at which they occur.
-values_for_peaks, times_of_peaks = get_peaks_and_times(amp_data, time)
+values_for_peaks, times_of_peaks, peak_indexes = get_peaks_and_times(amp_data, time)
+
 # Get where the ramps occur. 
-non_null_length_values, times_of_lengths = get_non_null_lengths_and_times(length_data, time)
+non_null_length_values, times_of_lengths, length_indexes = get_non_null_lengths_and_times(length_data, time)
+
+# Now find the peaks in range of the ramps. 
+peaks_during_ramp, good_peak_times = get_peaks_during_ramps(peak_indexes, length_indexes, amp_data, time)
 
 
-# Now, get the values of the signal for which the ramp occurs. 
-
+# TBC... now you have to do something with those peaks during the ramp... 
 
 
 # Graph our amplitude and inst_freq side-by-side.
@@ -120,6 +140,7 @@ ax0 = fig.add_subplot(311)
 ax0.set_title('Signal')
 ax0.plot(time[:], amp_data)
 ax0.plot(times_of_peaks, values_for_peaks, 'ro')
+ax0.plot(good_peak_times, peaks_during_ramp, 'go')
 
 # Length
 ax1 = fig.add_subplot(312)
